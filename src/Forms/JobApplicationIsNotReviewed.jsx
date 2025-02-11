2775245db53a2a06b841e48620f3a1e4434820cd
Yamini <yaminikaamaakshi@gmail.com>
2025-02-11 12:40:49 +0530

    Actions forms updated

--- src/Forms/JobApplicationIsNotReviewed.jsx
+++ src/Forms/JobApplicationIsNotReviewed.jsx
@@ -4,23 +4,23 @@ import DueDate from "./DefaultFields/DueDate.jsx";
 import AddAction from "./DefaultFields/AddAction.jsx";
 
 // eslint-disable-next-line react/prop-types
-const JobApplicationIsNotReviewed = ({ actionCode, handleFormSubmit,formData,webhooks }) => {
+const JobApplicationIsNotReviewed = ({ actionCode,formData,webhooks }) => {
 
     switch (actionCode) {
         case "JOB_APPLICATION_RECEIVED_SEND_WEBHOOK_NOTIFICATION":
             return (
-                <Form onFinish={handleFormSubmit}>
+               <>
                     <WebHooks webhooks={webhooks} />
                     <AddAction />
-                </Form>
+               </>
             )
 
         case 'JOB_APPLICATION_RECEIVED_ADD_TASK_TO_OWNER':
             return (
-                <Form onFinish={handleFormSubmit}>
+                <>
                     <DueDate formData={formData} />
                     <AddAction />
-                </Form>
+                </>
             )
         default:
             return ;
