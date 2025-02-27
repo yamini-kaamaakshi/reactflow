f51b32ee9b8eeae31434ad6b7058f15d54cd3da3
aruna <arunakamakshi09@gmail.com>
2025-02-27 18:30:36 +0530

    Added the ContactStatus Forms.

---
+++ src/Forms/CRM/ContactStatusIsUpdated.jsx
@@ -0,0 +1,37 @@
+import {useState} from "react";
+import {Message, SendAs, Subject, UserDropdown} from "../DefaultFields/FormFields.jsx";
+
+
+const ContactStatusIsUpdated = ({actionCode, users,formData}) => {
+    const [sendAs, setSendAs] = useState(formData?.sendAs || "DEFAULT");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+
+    switch (actionCode) {
+
+        case "CONTACT_STATUS_UPDATED_SEND_EMAIL_TO_CONTACT":
+        case "CONTACT_STATUS_UPDATED_SEND_EMAIL_TO_CONTACT_OWNER":
+            return (
+                <>
+                    <SendAs sendAs={sendAs} setSendAs={setSendAs} formData={formData}/>
+                    <Subject subject={subject} setSubject={setSubject} formData={formData}/>
+                    <Message message={message} setMessage={setMessage} formData={formData}/>
+                </>
+            );
+
+        case "CONTACT_STATUS_UPDATED_SEND_EMAIL_TO_SPECIFIED_USERS":
+            return (
+                <>
+                    <UserDropdown users={users} formData={formData}/>
+                    <SendAs sendAs={sendAs} setSendAs={setSendAs} formData={formData}/>
+                    <Subject subject={subject} setSubject={setSubject} formData={formData}/>
+                    <Message message={message} setMessage={setMessage} formData={formData}/>
+                </>
+            );
+
+        default:
+            return null;
+    }
+};
+
+export default ContactStatusIsUpdated;
