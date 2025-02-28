660b190bd2de0efd11fea0bfd0e986bfc7614765
aruna <arunakamakshi09@gmail.com>
2025-02-28 18:22:46 +0530

    Updated the Opportunity Forms.

--- src/Forms/CRM/OpportunityPipelineStatusIsUpdated.jsx
+++ src/Forms/CRM/OpportunityPipelineStatusIsUpdated.jsx
@@ -1,13 +1,22 @@
-import {Message, SendAsRadioButtons, Subject, UserDropdown} from "../DefaultFields/FormFields.jsx";
+import {Message, SendAsRadioButtons, SenderSelection, Subject, UserDropdown} from "../DefaultFields/FormFields.jsx";
+import {useState} from "react";
 
-const OpportunityPipelineStatusIsUpdated = ({actionCode, users,formData}) => {
+const OpportunityPipelineStatusIsUpdated = ({actionCode, users,formData,senders}) => {
+    const [sendAs, setSendAs] = useState("default");
+    const [selectedSender, setSelectedSender] = useState(null);
+
     switch (actionCode) {
-
         case "OPPORTUNITY_STATUS_UPDATED_SEND_EMAIL_TO_OPPORTUNITY":
         case "OPPORTUNITY_STATUS_UPDATED_SEND_EMAIL_TO_OPPORTUNITY_OWNER":
             return (
                 <>
-                    <SendAsRadioButtons/>
+                    <SendAsRadioButtons setSendAs={setSendAs} formData={formData}/>
+                    <SenderSelection
+                        sendAs={sendAs}
+                        selectedSender={selectedSender}
+                        setSelectedSender={setSelectedSender}
+                        senders={senders}
+                    />
                     <Subject formData={formData}/>
                     <Message formData={formData}/>
                 </>
@@ -17,7 +26,13 @@ const OpportunityPipelineStatusIsUpdated = ({actionCode, users,formData}) => {
             return (
                 <>
                     <UserDropdown users={users}/>
-                    <SendAsRadioButtons/>
+                    <SendAsRadioButtons setSendAs={setSendAs} formData={formData}/>
+                    <SenderSelection
+                        sendAs={sendAs}
+                        selectedSender={selectedSender}
+                        setSelectedSender={setSelectedSender}
+                        senders={senders}
+                    />
                     <Subject formData={formData}/>
                     <Message formData={formData}/>
                 </>
