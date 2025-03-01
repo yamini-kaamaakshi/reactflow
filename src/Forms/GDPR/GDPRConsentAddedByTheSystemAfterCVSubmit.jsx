0b21f5a104503e0190d83b1aaf3d89823cafe16f
aruna <arunakamakshi09@gmail.com>
2025-03-01 12:29:09 +0530

    Updated the GDPR Forms.

--- src/Forms/GDPR/GDPRConsentAddedByTheSystemAfterCVSubmit.jsx
+++ src/Forms/GDPR/GDPRConsentAddedByTheSystemAfterCVSubmit.jsx
@@ -1,12 +1,23 @@
-import {Subject, Message} from "../DefaultFields/FormFields.jsx"
+import {Subject, Message, SendAsRadioButtons, SenderSelection} from "../DefaultFields/FormFields.jsx"
+import {useState} from "react";
 
 
 // eslint-disable-next-line react/prop-types
-const GDPRConsentAddedByTheSystemAfterCVSubmit = ({ actionCode,formData }) => {
+const GDPRConsentAddedByTheSystemAfterCVSubmit = ({ actionCode,formData,senders}) => {
+    const [sendAs, setSendAs] = useState("default");
+    const [selectedSender, setSelectedSender] = useState(null);
+
     switch (actionCode) {
         case "SEND_EMAIL_AFTER_CV_SUBMIT":
             return (
                 <>
+                    <SendAsRadioButtons formData={formData} setSendAs={setSendAs}/>
+                    <SenderSelection
+                        sendAs={sendAs}
+                        selectedSender={selectedSender}
+                        setSelectedSender={setSelectedSender}
+                        senders={senders}
+                    />
                     <Subject formData={formData} />
                     <Message formData={formData} />
                 </>
