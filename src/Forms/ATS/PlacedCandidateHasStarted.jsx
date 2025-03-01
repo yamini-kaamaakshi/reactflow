e85203ecc7d19668127b9d2695b45d59d82c620e
aruna <arunakamakshi09@gmail.com>
2025-03-01 10:26:38 +0530

    Updated the ATS Forms.

--- src/Forms/ATS/PlacedCandidateHasStarted.jsx
+++ src/Forms/ATS/PlacedCandidateHasStarted.jsx
@@ -1,17 +1,34 @@
 import {Form, Input} from "antd";
-import {WebHooks,Subject,Message,WhenAfterDays} from "../DefaultFields/FormFields.jsx";
+import {
+    WebHooks,
+    Subject,
+    Message,
+    WhenAfterDays,
+    SendAsRadioButtons,
+    SenderSelection
+} from "../DefaultFields/FormFields.jsx";
+import {useState} from "react";
 
 
 // eslint-disable-next-line react/prop-types
-const PlacedCandidateHasStarted = ({ actionCode,webhooks,formData }) => {
+const PlacedCandidateHasStarted = ({ actionCode,webhooks,formData,senders}) => {
+    const [sendAs, setSendAs] = useState("default");
+    const [selectedSender, setSelectedSender] = useState(null);
+
     switch (actionCode) {
         case "ATS_PLACEMENT_STARTED_SEND_EMAIL_TO_USER":
         case "ATS_PLACEMENT_STARTED_SEND_EMAIL_TO_CLIENT":
         case "ATS_PLACEMENT_STARTED_SEND_EMAIL_TO_CANDIDATE":
             return (
                 <>
-                    {/*<FormFields formData={formData} />*/}
                     <WhenAfterDays formData={formData} />
+                    <SendAsRadioButtons formData={formData} setSendAs={setSendAs}/>
+                    <SenderSelection
+                        sendAs={sendAs}
+                        selectedSender={selectedSender}
+                        setSelectedSender={setSelectedSender}
+                        senders={senders}
+                    />
                     <Subject formData={formData} />
                     <Message formData={formData} />
                 </>
