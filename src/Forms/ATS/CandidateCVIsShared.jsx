119b154dae118089f29699b7bc82d1cdf4b6c4e3
aruna <arunakamakshi09@gmail.com>
2025-03-01 10:23:12 +0530

    Updated the ATS Forms.

--- src/Forms/ATS/CandidateCVIsShared.jsx
+++ src/Forms/ATS/CandidateCVIsShared.jsx
@@ -1,12 +1,23 @@
-import {Subject,Message} from "../DefaultFields/FormFields.jsx";
+import {Subject, Message, SendAsRadioButtons, SenderSelection} from "../DefaultFields/FormFields.jsx";
+import {useState} from "react";
 
 
 // eslint-disable-next-line react/prop-types
-const CandidateCVIsShared = ({ actionCode,formData }) => {
+const CandidateCVIsShared = ({ actionCode,formData,senders}) => {
+    const [sendAs, setSendAs] = useState("default");
+    const [selectedSender, setSelectedSender] = useState(null);
+
     switch (actionCode) {
         case "ATS_CANDIDATE_CV_SHARED_TO_CLIENT":
             return (
                 <>
+                    <SendAsRadioButtons formData={formData} setSendAs={setSendAs}/>
+                    <SenderSelection
+                        sendAs={sendAs}
+                        selectedSender={selectedSender}
+                        setSelectedSender={setSelectedSender}
+                        senders={senders}
+                    />
                     <Subject formData={formData} />
                     <Message formData={formData} />
 
