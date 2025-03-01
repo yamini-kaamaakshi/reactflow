aed07d545df9c0b3c475d480d0a116a4085c3d97
aruna <arunakamakshi09@gmail.com>
2025-03-01 11:36:31 +0530

    Updated the ATS Forms.

--- src/Forms/ATS/WhenAPlacementIsNearingItsEndDate.jsx
+++ src/Forms/ATS/WhenAPlacementIsNearingItsEndDate.jsx
@@ -12,7 +12,7 @@ import {useState} from "react";
 
 
 // eslint-disable-next-line react/prop-types
-const WhenAPlacementIsNearingItsEndDate = ({ actionCode,formData,webhooks,senders}) => {
+const WhenAPlacementIsNearingItsEndDate = ({actionCode, formData, webhooks, senders}) => {
     const [sendAs, setSendAs] = useState("default");
     const [selectedSender, setSelectedSender] = useState(null);
 
@@ -20,7 +20,7 @@ const WhenAPlacementIsNearingItsEndDate = ({ actionCode,formData,webhooks,sender
         case "ATS_PLACEMENT_ABOUT_END_SEND_EMAIL_TO_USER":
             return (
                 <>
-                <WhenBeforeDays formData = {formData} />
+                    <WhenBeforeDays formData={formData}/>
                     <SendAsRadioButtons formData={formData} setSendAs={setSendAs}/>
                     <SenderSelection
                         sendAs={sendAs}
@@ -28,44 +28,44 @@ const WhenAPlacementIsNearingItsEndDate = ({ actionCode,formData,webhooks,sender
                         setSelectedSender={setSelectedSender}
                         senders={senders}
                     />
-                <Subject formData={formData} />
-                <Message formData={formData} />
+                    <Subject formData={formData}/>
+                    <Message formData={formData}/>
                 </>
             );
 
         case "ATS_PLACEMENT_ABOUT_END_ADD_CANDIDATE_TO_SEQUENCE":
             return (
                 <>
-                    <WhenBeforeDays formData = {formData} />
+                    <WhenBeforeDays formData={formData}/>
                 </>
             );
 
         case "ATS_PLACEMENT_ABOUT_END_ADD_TASK_TO_OWNER":
             return (
                 <>
-                    <WhenBeforeDays formData = {formData} />
-                    <DueDay formData={formData} />
-                    <Subject formData={formData} />
-                    <Message formData={formData} />
+                    <WhenBeforeDays formData={formData}/>
+                    <DueDay formData={formData}/>
+                    <Subject formData={formData}/>
+                    <Message formData={formData}/>
                 </>
             );
 
         case "ATS_PLACEMENT_ABOUT_END_SEND_WEBHOOK_NOTIFICATION":
             return (
                 <>
-                    <WhenBeforeDays formData = {formData} />
-                    <WebHooks webhooks={webhooks} formData={formData} />
+                    <WhenBeforeDays formData={formData}/>
+                    <WebHooks webhooks={webhooks} formData={formData}/>
                 </>
             );
 
         case "ATS_PLACEMENT_ABOUT_END_SEND_SMS_TO_CANDIDATE":
             return (
                 <>
-                    <WhenBeforeDays formData = {formData} />
+                    <WhenBeforeDays formData={formData}/>
                     <Form.Item
                         label="Message:"
                         name="message"
-                        rules={[{ required: true}]}
+                        rules={[{required: true}]}
                         initialValue={formData?.message}
                     >
                         <div className="input-group">
@@ -83,24 +83,23 @@ const WhenAPlacementIsNearingItsEndDate = ({ actionCode,formData,webhooks,sender
             );
 
         case "ATS_PLACEMENT_ABOUT_END_SEND_EMAIL_TO_CLIENT":
-            return (
-                <>
-                    <WhenBeforeDays formData = {formData} />
-                    <Subject formData={formData} />
-                    <Message formData={formData} />
-                </>
-            );
-
         case "ATS_PLACEMENT_ABOUT_END_SEND_EMAIL_TO_CANDIDATE":
             return (
                 <>
-                    <WhenBeforeDays formData = {formData} />
-                    <Subject formData={formData} />
-                    <Message formData={formData} />
+                    <WhenBeforeDays formData={formData}/>
+                    <SendAsRadioButtons formData={formData} setSendAs={setSendAs}/>
+                    <SenderSelection
+                        sendAs={sendAs}
+                        selectedSender={selectedSender}
+                        setSelectedSender={setSelectedSender}
+                        senders={senders}
+                    />
+                    <Subject formData={formData}/>
+                    <Message formData={formData}/>
                 </>
             );
         default:
-            return ;
+            return;
     }
 };
 
