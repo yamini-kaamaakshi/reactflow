cb862361b69c20a61dda6c2f83aab0fce4143260
aruna <arunakamakshi09@gmail.com>
2025-03-01 10:17:16 +0530

    Updated the ATS Forms.

--- src/Forms/ATS/JobInterviewIsDue.jsx
+++ src/Forms/ATS/JobInterviewIsDue.jsx
@@ -1,8 +1,12 @@
-import {Subject,Message,WhenDaysBefore} from "../DefaultFields/FormFields.jsx";
+import {Subject, Message, WhenDaysBefore, SendAsRadioButtons, SenderSelection} from "../DefaultFields/FormFields.jsx";
+import {useState} from "react";
 
 
 // eslint-disable-next-line react/prop-types
-const JobInterviewIsDue = ({ actionCode,formData }) => {
+const JobInterviewIsDue = ({ actionCode,formData,senders}) => {
+    const [sendAs, setSendAs] = useState("default");
+    const [selectedSender, setSelectedSender] = useState(null);
+
     switch (actionCode) {
         case "JOB_INTERVIEW_DUE_SEND_SMS_TO_CLIENT":
         case "JOB_INTERVIEW_DUE_SEND_SMS_TO_CANDIDATE":
@@ -19,6 +23,13 @@ const JobInterviewIsDue = ({ actionCode,formData }) => {
             return (
                 <>
                     <WhenDaysBefore formData={formData} />
+                    <SendAsRadioButtons formData={formData} setSendAs={setSendAs}/>
+                    <SenderSelection
+                        sendAs={sendAs}
+                        selectedSender={selectedSender}
+                        setSelectedSender={setSelectedSender}
+                        senders={senders}
+                    />
                     <Subject formData={formData} />
                     <Message formData={formData} />
 
