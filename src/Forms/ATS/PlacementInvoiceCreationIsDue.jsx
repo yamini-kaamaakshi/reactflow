eee4b49bdd9595198532f92b1dc29b360ac208d5
aruna <arunakamakshi09@gmail.com>
2025-03-01 11:30:30 +0530

    Updated the ATS Forms.

--- src/Forms/ATS/PlacementInvoiceCreationIsDue.jsx
+++ src/Forms/ATS/PlacementInvoiceCreationIsDue.jsx
@@ -1,18 +1,36 @@
-import {DaysBeforeBillingDate,Subject} from "../DefaultFields/FormFields.jsx";
+import {
+    DaysBeforeBillingDate,
+    Message,
+    SendAsRadioButtons,
+    SenderSelection,
+    Subject
+} from "../DefaultFields/FormFields.jsx";
+import {useState} from "react";
 
 
 // eslint-disable-next-line react/prop-types
-const PlacementInvoiceCreationIsDue = ({ actionCode,formData }) => {
+const PlacementInvoiceCreationIsDue = ({actionCode, formData,senders}) => {
+    const [sendAs, setSendAs] = useState("default");
+    const [selectedSender, setSelectedSender] = useState(null);
+
     switch (actionCode) {
         case "SEND_INVOICE_REMINDER_EMAIL_TO_PLACEMENT_OWNER":
             return (
                 <>
-                <DaysBeforeBillingDate formData = {formData} />
-                <Subject formData={formData} />
+                    <DaysBeforeBillingDate formData={formData}/>
+                    <SendAsRadioButtons formData={formData} setSendAs={setSendAs}/>
+                    <SenderSelection
+                        sendAs={sendAs}
+                        selectedSender={selectedSender}
+                        setSelectedSender={setSelectedSender}
+                        senders={senders}
+                    />
+                    <Subject formData={formData}/>
+                    <Message formData={formData} />
                 </>
             );
         default:
-            return ;
+            return;
     }
 };
 
