2dffd142847597db977370764b573771e15c7256
aruna <arunakamakshi09@gmail.com>
2025-03-01 11:40:19 +0530

    Updated the ATS Forms.

--- src/Forms/ATS/CandidatePipelineStatusIsUpdated.jsx
+++ src/Forms/ATS/CandidatePipelineStatusIsUpdated.jsx
@@ -1,12 +1,23 @@
-import {WebHooks,Subject,Message} from "../DefaultFields/FormFields.jsx";
+import {useState} from "react";
+import {WebHooks, Subject, Message, SendAsRadioButtons, SenderSelection} from "../DefaultFields/FormFields.jsx";
 
 
 // eslint-disable-next-line react/prop-types
-const CandidatePipelineStatusIsUpdated = ({ actionCode,webhooks,formData }) => {
+const CandidatePipelineStatusIsUpdated = ({ actionCode,webhooks,formData,senders}) => {
+    const [sendAs, setSendAs] = useState("default");
+    const [selectedSender, setSelectedSender] = useState(null);
+
     switch (actionCode) {
         case "CANDIDATE_PIPELINE_STATUS_UPDATED_SEND_EMAIL_TO_CANDIDATE":
             return (
                 <>
+                    <SendAsRadioButtons formData={formData} setSendAs={setSendAs}/>
+                    <SenderSelection
+                        sendAs={sendAs}
+                        selectedSender={selectedSender}
+                        setSelectedSender={setSelectedSender}
+                        senders={senders}
+                    />
                     <Subject formData={formData} />
                     <Message formData={formData} />
                 </>
