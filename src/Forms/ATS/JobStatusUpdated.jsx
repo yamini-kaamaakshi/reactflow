af8b2e6caf00ac3031d4f6a8ba0b389e186e0280
aruna <arunakamakshi09@gmail.com>
2025-03-01 11:56:19 +0530

    Updated the WorkFlow.

--- src/Forms/ATS/JobStatusUpdated.jsx
+++ src/Forms/ATS/JobStatusUpdated.jsx
@@ -1,26 +1,29 @@
-import {Subject,Message} from "../DefaultFields/FormFields.jsx";
+import {useState} from "react";
+import {Subject, Message, SendAsRadioButtons, SenderSelection} from "../DefaultFields/FormFields.jsx";
 
 
 // eslint-disable-next-line react/prop-types
-const JobStatusUpdated = ({ actionCode,formData }) => {
+const JobStatusUpdated = ({ actionCode,formData,senders}) => {
+    const [sendAs, setSendAs] = useState("default");
+    const [selectedSender, setSelectedSender] = useState(null);
+
     switch (actionCode) {
         case "JOB_STATUS_UPDATED_SEND_EMAIL_TO_JOB_OWNER":
+        case "JOB_STATUS_UPDATED_SEND_EMAIL_TO_CLIENT":
             return (
                 <>
+                    <SendAsRadioButtons formData={formData} setSendAs={setSendAs}/>
+                    <SenderSelection
+                        sendAs={sendAs}
+                        selectedSender={selectedSender}
+                        setSelectedSender={setSelectedSender}
+                        senders={senders}
+                    />
                     <Subject formData={formData} />
                     <Message formData={formData} />
 
                 </>
             );
-
-        case "JOB_STATUS_UPDATED_SEND_EMAIL_TO_CLIENT":
-            return (
-                <>
-                  <Subject formData={formData} />
-                    <Message formData={formData} />
-
-                </>
-            )
         default:
             return ;
     }
