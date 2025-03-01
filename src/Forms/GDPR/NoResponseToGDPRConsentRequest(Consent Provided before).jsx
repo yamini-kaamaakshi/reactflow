93b49ff098da1b4d049a4b2c10811e875ed109cd
aruna <arunakamakshi09@gmail.com>
2025-03-01 12:20:59 +0530

    Updated the GDPR Forms.

--- src/Forms/GDPR/NoResponseToGDPRConsentRequest(Consent Provided before).jsx
+++ src/Forms/GDPR/NoResponseToGDPRConsentRequest(Consent Provided before).jsx
@@ -1,8 +1,18 @@
-import {Subject, Message, WhenDaysAfterOriginalRequest} from "../DefaultFields/FormFields.jsx"
+import {
+    Subject,
+    Message,
+    WhenDaysAfterOriginalRequest,
+    SendAsRadioButtons,
+    SenderSelection
+} from "../DefaultFields/FormFields.jsx"
+import {useState} from "react";
 
 
 // eslint-disable-next-line react/prop-types
-const NoResponseToGDPRConsentRequestConsentProvidedBefore = ({ actionCode,formData }) => {
+const NoResponseToGDPRConsentRequestConsentProvidedBefore = ({ actionCode,formData,senders}) => {
+    const [sendAs, setSendAs] = useState("default");
+    const [selectedSender, setSelectedSender] = useState(null);
+
     switch (actionCode) {
         case "GDPR_NO_RESPONSE_MARK_AS_DNC_AND_ARCHIVE_EXISTING":
             return (
@@ -14,6 +24,13 @@ const NoResponseToGDPRConsentRequestConsentProvidedBefore = ({ actionCode,formDa
             return (
                 <>
                     <WhenDaysAfterOriginalRequest formData={formData} />
+                    <SendAsRadioButtons formData={formData} setSendAs={setSendAs}/>
+                    <SenderSelection
+                        sendAs={sendAs}
+                        selectedSender={selectedSender}
+                        setSelectedSender={setSelectedSender}
+                        senders={senders}
+                    />
                     <Subject formData={formData} />
                     <Message formData={formData} />
                 </>
