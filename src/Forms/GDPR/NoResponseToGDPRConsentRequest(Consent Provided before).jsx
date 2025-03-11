6f1057bb59c93412da4c75df31ce987be0afdfd6
Tejesh <tejesh@recruitly.io>
2025-03-11 14:22:22 +0530

    fetchEmailSequences

--- src/Forms/GDPR/NoResponseToGDPRConsentRequest(Consent Provided before).jsx
+++ src/Forms/GDPR/NoResponseToGDPRConsentRequest(Consent Provided before).jsx
@@ -1,30 +1,43 @@
+import { useState } from "react";
 import {
     Subject,
     Message,
     WhenDaysAfterOriginalRequest,
     SendAsRadioButtons,
     SenderSelection
-} from "../DefaultFields/FormFields.jsx"
-import {useState} from "react";
-
+} from "../DefaultFields/FormFields.jsx";
+import {Form, Input, Switch} from "antd";
 
 // eslint-disable-next-line react/prop-types
-const NoResponseToGDPRConsentRequestConsentProvidedBefore = ({ actionCode,formData,senders}) => {
+const NoResponseToGDPRConsentRequestConsentProvidedBefore = ({ actionCode, formData, senders }) => {
     const [sendAs, setSendAs] = useState("default");
     const [selectedSender, setSelectedSender] = useState(null);
+    const [notifyOwner, setNotifyOwner] = useState(false);
+
+    const handleCheckboxChange = (checked) => {
+        setNotifyOwner(checked);
+    };
 
     switch (actionCode) {
         case "GDPR_NO_RESPONSE_MARK_AS_DNC_AND_ARCHIVE_EXISTING":
             return (
                 <>
                     <WhenDaysAfterOriginalRequest formData={formData} />
+                    <Form.Item label="If Consent Expiry is less than">
+                        <Input type="number" min={1} defaultValue={7} addonAfter="Days" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Switch checked={notifyOwner} onChange={handleCheckboxChange} style={{ marginRight: 8 }} />
+                        <span>Notify the owner after record is archived</span>
+                    </Form.Item>
                 </>
             );
+
         case "NO_RESPONSE_TO_CONSENT_REQUEST_EXISTING":
             return (
                 <>
                     <WhenDaysAfterOriginalRequest formData={formData} />
-                    <SendAsRadioButtons formData={formData} setSendAs={setSendAs}/>
+                    <SendAsRadioButtons formData={formData} setSendAs={setSendAs} />
                     <SenderSelection
                         sendAs={sendAs}
                         selectedSender={selectedSender}
@@ -35,26 +48,51 @@ const NoResponseToGDPRConsentRequestConsentProvidedBefore = ({ actionCode,formDa
                     <Message formData={formData} />
                 </>
             );
+
         case "GDPR_NO_RESPONSE_MARK_AS_DNC_EXISTING":
             return (
                 <>
                     <WhenDaysAfterOriginalRequest formData={formData} />
+                    <Form.Item label="If Consent Expiry is less than">
+                        <Input type="number" min={1} defaultValue={7} addonAfter="Days" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Switch checked={notifyOwner} onChange={handleCheckboxChange} style={{ marginRight: 8 }} />
+                        <span>Notify the owner after record is marked as DNC</span>
+                    </Form.Item>
                 </>
             );
+
         case "GDPR_NO_RESPONSE_ANONYMISE_AND_ARCHIVE_EXISTING":
             return (
                 <>
                     <WhenDaysAfterOriginalRequest formData={formData} />
+                    <Form.Item label="If Consent Expiry is less than">
+                        <Input type="number" min={1} defaultValue={7} addonAfter="Days" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Switch checked={notifyOwner} onChange={handleCheckboxChange} style={{ marginRight: 8 }} />
+                        <span>Notify the owner after record is anonymized</span>
+                    </Form.Item>
                 </>
             );
+
         case "GDPR_NO_RESPONSE_DELETE_RECORD_EXISTING":
             return (
                 <>
                     <WhenDaysAfterOriginalRequest formData={formData} />
+                    <Form.Item label="If Consent Expiry is less than">
+                        <Input type="number" min={1} defaultValue={7} addonAfter="Days" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Switch checked={notifyOwner} onChange={handleCheckboxChange} style={{ marginRight: 8 }} />
+                        <span>Notify the owner after record is deleted</span>
+                    </Form.Item>
                 </>
             );
+
         default:
-            return ;
+            return null;
     }
 };
 
